d6061d83b8ffcb9c16b5568added42a4409b0fdb
AIAgents4r3alm <AIAgents@r3alm.com>
2025-11-07 14:26:49 -0500

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import { BrowserRouter } from 'react-router-dom';
 import { ModelProvider } from './contexts/ModelContext';
 import { UserModeProvider } from './contexts/UserModeContext';
 import { ThemeProvider } from './contexts/ThemeContext';
-import { AuthProvider } from './contexts/AuthContext'; // New
+import { AuthProvider } from './contexts/AuthContext';
 import AppRoutes from './routes';
 
 export default function App() {
   return (
     <BrowserRouter>
-      <AuthProvider> {/* New: Wrap for session management */}
+      <AuthProvider>
         <ThemeProvider>
           <ModelProvider>
             <UserModeProvider>
